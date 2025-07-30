896131444bdced8f90c9f0ddeff554536a05475a
kayloshai <kayloshai@gmai.com>
2025-07-30 15:30:58 +0200

    Refactor Home component to use Link for navigation and remove unused Props interface

--- src/app/pages/home.tsx
+++ src/app/pages/home.tsx
@@ -1,7 +1,5 @@
 import { Link } from "react-router-dom";
 
-interface Props { }
-
 export const Home = ({ }: Props) => {
     return (
         <div id="home-page" className="container py-5">
@@ -11,7 +9,7 @@ export const Home = ({ }: Props) => {
                     This is a placeholder home page. Use the navigation menu to explore services, get instant quotes, or sign up for an account.
                 </p>
                 <div className="d-flex justify-content-center gap-3">
-                    <a href="/trade-locator/specials" className="btn btn-primary">View Specials</a>
+                    <Link to="/specials" className="btn btn-primary">View Specials</Link>
                     <Link to="/quote" className="btn btn-outline-secondary">Get a Quote</Link>
                 </div>
             </div>
